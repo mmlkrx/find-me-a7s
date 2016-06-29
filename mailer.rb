34b88f8bbaf1d153b779f5a488f135ddb8c0cc81
Matthias Loker <mmloker@gmail.com>
2016-06-30 00:40:47 +0200

    Use global variable to workaround passing mail body

--- mailer.rb
+++ mailer.rb
@@ -3,16 +3,19 @@ require 'mail'
 class Mailer
   attr_accessor :email_body
 
+  def initialize
+    Mail.defaults do
+      delivery_method :sendmail
+    end
+  end
+
   def deliver
-    mail = Mail.new do
+    $fu_mail_gem = email_body
+    Mail.deliver do
       from    'myawesome@scraper.bot'
       to      'mmloker@gmail.com'
       subject "Looks like I've found something"
-      body    email_body
+      body    $fu_mail_gem
     end
-
-    mail.delivery_method :sendmail
-
-    mail.deliver
   end
 end
