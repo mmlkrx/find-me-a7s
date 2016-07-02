bca3a9f19de3034523557a57df5c97ada2e02932
Matthias Loker <mmloker@gmail.com>
2016-07-02 15:13:04 +0200

    Fix bug when first url is nil

--- url_dump.rb
+++ url_dump.rb
@@ -7,9 +7,10 @@ class UrlDump
 
   def self.find(url)
     File.open('urls.dump', 'a+') do |file|
-      file.lazy.select do |line|
-        line.include?(url)
-      end.first[0..-2]
+      url = file.lazy.select do |line|
+              line.include?(url)
+            end.first
+      url.nil? ? nil : url[0..-2]
     end
   end
 end
